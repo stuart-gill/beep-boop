fe5050819d60ecc51cb5053711e9903d15ba45c6
Stuart Gill <stuart.a.gill@gmail.com>
2018-10-26 10:14:44 -0700

    Create map function to convert numbers divisible by three to var 'dave' with dave string

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,16 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
     var input = $("#userInput").val();
-    var result = [];
+    var enumerated = [];
+    var dave = "I'm sorry Dave";
     for (i=0; i<=input; i++){
-      result.push(i);
+      enumerated.push(i);
     }
+    var result = enumerated.map((v,i,a) => {
+      return v%3===0? v=dave:v;
+    })
     console.log(result);
+
     // $("#story").append("<p>"+result+"</p>");
     // $("#story").show();
   });
