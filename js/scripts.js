91099ac5f039a273a12a40b51fb603a414525a81
Stuart Gill <stuart.a.gill@gmail.com>
2018-10-26 10:00:04 -0700

    Create functioning counter in JS file

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
-
-
-    $("#story").append("<p>"+result+"</p>");
-    $("#story").show();
+    var input = $("#userInput").val();
+    var result = [];
+    for (i=0; i<=input; i++){
+      result.push(i);
+    }
+    console.log(result);
+    // $("#story").append("<p>"+result+"</p>");
+    // $("#story").show();
   });
 });
