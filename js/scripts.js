e4bd569ab6bbc1e5d50c8bc6bde9577335ab31b8
Stuart Gill <stuart.a.gill@gmail.com>
2018-10-26 11:39:09 -0700

    Attempt to separate business and front end logic and simply code as much as possible.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,29 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    event.preventDefault();
+    event.preventDefault()
     var input = $("#userInput").val();
     var enumerated = [];
-    var dave = "I'm sorry Dave";
     for (i=0; i<=input; i++){
       enumerated.push(i);
     }
+
+
     var result = enumerated.map(function(v) {
       if (v%3===0){
-        return v=dave;
+        return v="I'm sorry, Dave. I'm afraid I can't do that.";
       } else{
         digits = v.toString().split('');
         if (digits.includes('1')){
-          return v='boop'
+          return v='Boop!'
         } else if (digits.includes('0')){
-          return v='beep'
+          return v='Beep!'
         } else{
           return v;
         }
       }
     })
-    console.log(result);
 
-
-    // var num = 123456;
-    // var digits = num.toString().split('');
-    // var realDigits = digits.map(Number)
-    // console.log(realDigits);
-    // $("#story").append("<p>"+result+"</p>");
-    // $("#story").show();
+    $("#story").append("<p>"+result.join(' ')+"</p>");
+    $("#story").show();
   });
 });
