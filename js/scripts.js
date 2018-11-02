666fc6b117353b09e9b35327f88891849234b05a
Stuart Gill <stuart.a.gill@gmail.com>
2018-11-02 15:21:29 -0700

    properly separate user interface from business logic by encapsulating business logic in a function

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,34 @@
-$(document).ready(function() {
-  $("#form").submit(function(event) {
-    event.preventDefault()
+
+
+
+var translater = function (inputtedNumber, inputtedName){
+  var enumerated = [];
+  for (i = 0; i <= inputtedNumber; i++) {
+    enumerated.push(i);
+  }
+  var translatedArray = enumerated.map(function (v) {
+    if (v % 3 === 0) {
+      return v = "I'm sorry " + inputtedName + ", I'm afraid I can't do that.";
+    } else {
+      digits = v.toString().split('');
+      if (digits.includes('1')) {
+        return v = 'Boop!';
+      } else if (digits.includes('0')) {
+        return v = 'Beep!';
+      } else {
+        return v;
+      }
+    }
+  })
+  return translatedArray;
+}
+
+$(document).ready(function () {
+  $("#form").submit(function (event) {
+    event.preventDefault();
     var userInput = $("#userInput").val();
     var userName = $("#userName").val();
-
-var enumerated = [];
-for (i=0; i<=userInput; i++){
-  enumerated.push(i);
-}
-var translatedArray = enumerated.map(function(v) {
-  if (v%3===0){
-    return v="I'm sorry " + userName + " I'm afraid I can't do that.";
-  } else{
-    digits = v.toString().split('');
-    if (digits.includes('1')){
-      return v='Boop!'
-    } else if (digits.includes('0')){
-      return v='Beep!'
-    } else{
-      return v;
-    }
-  }
-})
-
-    $("#result, p").text(translatedArray.join(' '));
+    $("#result, p").text(translater(userInput, userName).join(' '));
     $("#result").show();
   });
 });
